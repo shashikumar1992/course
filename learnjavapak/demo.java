b7707ad5c6902b7e3a5fc6c84b1ce2ddd395cdc2
shashikumar1992 <shashikumar.ss72@gmail.com>
2023-06-01 23:39:28 +0530

    Demo

---
+++ learnjavapak/demo.java
@@ -0,0 +1,18 @@
+package learnjavapak;
+
+import java.util.Map;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("Student name :");
+
+        String [] stud={"shashikumar","Dhavan kumar","Kiran KUmar"};
+
+         for(String display:stud)
+        {
+            System.out.println(display);
+        }
+
+
+    }
+}
